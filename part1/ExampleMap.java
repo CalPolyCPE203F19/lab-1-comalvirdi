af81d8d371988ee7073341e2b87e0307ab03a492
Comal Virdi <covirdi@unix1.csc.calpoly.edu>
2019-09-27 14:59:54 -0700

    fixed map

--- part1/ExampleMap.java
+++ part1/ExampleMap.java
@@ -7,26 +7,25 @@ class ExampleMap
    public static List<String> highEnrollmentStudents(
       Map<String, List<Course>> courseListsByStudentName, int unitThreshold)
    {
-      List<String> overEnrolledStudents = new LinkedList<>();
 
-      /*
-         Build a list of the names of students currently enrolled
-         in a number of units strictly greater than the unitThreshold.
-      */
+      List<String> overEnrolledStudents = new LinkedList<>();
+      int unitCount = 0;
+
       for (Map.Entry<String, List<Course>> student: courseListsByStudentName.entrySet()){
-         List getList = student.getValue();
-         int size = getList.size();
-         // System.out.println(student.getKey());
-         // System.out.println(student.getValue());
-         // System.out.println(getList.size());
-         if (unitThreshold < 4*size){
+         unitCount = 0;
+
+         for (Course c: student.getValue()) {
+            unitCount += c.getNumUnits();
+         }
+         
+         
+         if (unitThreshold < unitCount){
             overEnrolledStudents.add(student.getKey());
             
-
          }
       }
-
       
       return overEnrolledStudents;      
-   }
+      
+   }  
 }
